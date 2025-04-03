12892a38870f6a0ca7330bdb1353f513082479c0
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-04-03 16:54:38 +0530

    Update minimal-cost.cpp

--- Difficulty: Medium/Minimal Cost/minimal-cost.cpp
+++ Difficulty: Medium/Minimal Cost/minimal-cost.cpp
@@ -1,44 +1,69 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
   
-    int minimialCostSolve(int k, vector<int>& arr,int index,vector<int>&dp ){
-         if(index==0) return 0;
-         if(dp[index]!=-1) return dp[index];
-         
-         int min_steps=INT_MAX;
-         
-         //j->jumps
-         for(int j=1;j<=k;j++){
-             if(index-j>=0){
-                 int jump=minimialCostSolve(k,arr,index-j,dp)+abs(arr[index]-arr[index-j]);
-                 
-                 min_steps=min(min_steps,jump);
-             }
-         }
-         
-         return dp[index]=min_steps;
-    }
+
    
     int minimizeCost(int k, vector<int>& arr) {
         // Code here
         
-        int n=arr.size();
-        vector<int>dp(n+1,-1);
+        int n=arr.size();         //syntax
+        vector<int>dp(n,-1);     //syntax
+        dp[0]=0;                 //syntax
         
-        return minimialCostSolve(k,arr,n-1,dp);
+        
+        
+         
+        for(int i=1;i<n;i++){        //syntax
+            int minSteps=INT_MAX;     
+            for(int j=1;j<=k;j++){
+              if(i-j>=0){
+                  int jump=dp[i-j]+abs(arr[i]-arr[i-j]);
+                  minSteps=min(minSteps,jump);
+              }
+              
+              dp[i]=minSteps;   //syntax
+            }
+        }
+        
+        return dp[n-1];   //suntax
     }
 };
 
 
 
-   // RECURSIVE
+//              //memoization
+//     int minimialCostSolve(int k, vector<int>& arr,int index,vector<int>&dp ){
+//          if(index==0) return 0;
+//          if(dp[index]!=-1) return dp[index];
+         
+//          int min_steps=INT_MAX;
+         
+//          //j->jumps
+//          for(int j=1;j<=k;j++){
+//              if(index-j>=0){
+//                  int jump=minimialCostSolve(k,arr,index-j,dp)+abs(arr[index]-arr[index-j]);
+                 
+//                  min_steps=min(min_steps,jump);
+//              }
+//          }
+         
+//          return dp[index]=min_steps;
+//     }
+   
+//     int minimizeCost(int k, vector<int>& arr) {
+//         // Code here
+        
+//         int n=arr.size();
+//         vector<int>dp(n+1,-1);
+        
+//         return minimialCostSolve(k,arr,n-1,dp);
+//     }
+    
+    
+    
+    
+
+//    // RECURSIVE
 // int minimialCostSolve(int k, vector<int>& arr,int n ){
 //          if(n==0) return 0;
          
@@ -64,35 +89,3 @@ class Solution {
 //         return minimialCostSolve(k,arr,n);
 //     }
 
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        int res = obj.minimizeCost(k, arr);
-        cout << res << endl;
-        cout << "~" << endl;
-        // string tl;
-        // getline(cin, tl);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
