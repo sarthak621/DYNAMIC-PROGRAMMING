c5d1bf47cc178cdcbaa640ae93eff968bb59f0f3
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-04-15 17:43:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+  
+    int f(int i,int j, vector<int> &arr,vector<vector<int>>&dp){
+        //base case
+        if(i==j) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        //R.R
+        int mini=1e9;
+        for(int k=i;k<j;k++){
+            int steps=arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp);
+            mini=min(mini,steps);
+        }
+        
+        return dp[i][j]= mini;
+    }
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return f(1,n-1,arr,dp);
+    }
+};
+
+
+
+
+
+
+// //recursive
+//      int f(int i,int j, vector<int> &arr){
+//         //base case
+//         if(i==j) return 0;
+        
+//         //R.R
+//         int mini=1e9;
+//         for(int k=i;k<j;k++){
+//             int steps=arr[i-1]*arr[k]*arr[j] + f(i,k,arr) + f(k+1,j,arr);
+//             mini=min(mini,steps);
+//         }
+        
+//         return mini;
+//     }
+//     int matrixMultiplication(vector<int> &arr) {
+//         // code here
+//         int n=arr.size();
+        
+//         return f(1,n-1,arr);
+//     }
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
+    while (t--) {
+        int n;
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
+        Solution obj;
+        cout << obj.matrixMultiplication(a) << endl << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
