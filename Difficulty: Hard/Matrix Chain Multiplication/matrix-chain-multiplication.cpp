429369810f0792eb86bea5ae8487bab4fd75b668
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-04-16 22:55:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -8,28 +8,72 @@ using namespace std;
 class Solution {
   public:
   
-    int f(int i,int j, vector<int> &arr,vector<vector<int>>&dp){
-        //base case
-        if(i==j) return 0;
+    // int f(int i,int j, vector<int> &arr,vector<vector<int>>&dp){
+    //     //base case
+    //     if(i==j) return 0;
         
-        if(dp[i][j]!=-1) return dp[i][j];
-        //R.R
-        int mini=1e9;
-        for(int k=i;k<j;k++){
-            int steps=arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp);
-            mini=min(mini,steps);
-        }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     //R.R
+    //     int mini=1e9;
+    //     for(int k=i;k<j;k++){
+    //         int steps=arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp);
+    //         mini=min(mini,steps);
+    //     }
         
-        return dp[i][j]= mini;
-    }
+    //     return dp[i][j]= mini;
+    // }
     int matrixMultiplication(vector<int> &arr) {
         // code here
         int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(1,n-1,arr,dp);
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        // return f(1,n-1,arr,dp);
+        
+        //1.base case
+        for(int i=0;i<n;i++){
+            dp[i][i]=0;
+        }
+        
+        //2. for loops
+        for(int i=n-1;i>=1;i--){
+            for(int j=i+1;j<n;j++){
+               int mini=1e9;
+               for(int k=i;k<j;k++){
+               int steps=arr[i-1]*arr[k]*arr[j] + dp[i][k]+ dp[k+1][j];
+               mini=min(mini,steps);
+            }
+            dp[i][j]= mini;
+          }
+          
+        }
+        
+        return dp[1][n-1];
     }
+    
 };
 
+
+
+// // memoization
+//     int f(int i,int j, vector<int> &arr,vector<vector<int>>&dp){
+//         //base case
+//         if(i==j) return 0;
+        
+//         if(dp[i][j]!=-1) return dp[i][j];
+//         //R.R
+//         int mini=1e9;
+//         for(int k=i;k<j;k++){
+//             int steps=arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp);
+//             mini=min(mini,steps);
+//         }
+        
+//         return dp[i][j]= mini;
+//     }
+//     int matrixMultiplication(vector<int> &arr) {
+//         // code here
+//         int n=arr.size();
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+//         return f(1,n-1,arr,dp);
+//     }
 
 
 
